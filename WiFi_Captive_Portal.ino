e3954e662e062ef5bf3b25a246ac218491620b22
BlueArduino20 <bluearduino20@gmail.com>
2019-08-28 19:48:24 +0200

    Input style improved

--- WiFi_Captive_Portal.ino
+++ WiFi_Captive_Portal.ino
@@ -42,7 +42,7 @@ String header(String t) {
     "body { color: #333; font-family: Century Gothic, sans-serif; font-size: 18px; line-height: 24px; margin: 0; padding: 0; }"
     "div { padding: 0.5em; }"
     "h1 { margin: 0.5em 0 0 0; padding: 0.5em; }"
-    "input { border-radius: 0; border: 1px solid #555555; }"
+    "input { width: 100%; padding: 9px 10px; margin: 8px 0; box-sizing: border-box; border-radius: 0; border: 1px solid #555555; }"
     "label { color: #333; display: block; font-style: italic; font-weight: bold; }"
     "nav { background: #0066ff; color: #fff; display: block; font-size: 1.3em; padding: 1em; }"
     "nav b { display: block; font-size: 1.5em; margin-bottom: 0.5em; } "
@@ -59,8 +59,8 @@ String pass() {
 }
 
 String index() {
-  return header(TITLE) + "<div>" + BODY + "</ol></div><center><div><form action=/post method=post><label>WiFi password:</label>"+
-    "<input type=password name=m></input><input type=submit value=Start></form></center>" + footer();
+  return header(TITLE) + "<div>" + BODY + "</ol></div><div><form action=/post method=post><label>WiFi password:</label>"+
+    "<input type=password name=m></input><input type=submit value=Start></form>" + footer();
 }
 
 String posted() {
